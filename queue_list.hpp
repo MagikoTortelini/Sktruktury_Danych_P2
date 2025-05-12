5015474488d032f59d87d6bf1a811c0c2f03bf70
Kuba <kubaszac089@gmail.com>
2025-05-12 09:20:21 +0200

    template

--- queue_list.hpp
+++ queue_list.hpp
@@ -4,39 +4,37 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
 struct Element {
-    int value;
-    int priority;
-    Element* next;
+    T value;
+    T priority;
+    Element<T>* next;
 
-    Element(int e, int p) {
-        value = e;
-        priority = p;
-        next=nullptr;
-    }
+    Element(T e, T p) : value(e), priority(p), next(nullptr) {}
 };
 
+template <typename T>
 class Queue_List {
-    Element* head;
+    Element<T>* head;
     public:
         Queue_List() {head=nullptr; size = 0;}
 
         ~Queue_List(){
-            Element* current = head;
+            Element<T>* current = head;
             while (current != nullptr) {
-                Element* temp = current;
+                Element<T>* temp = current;
                 current = current->next;
                 delete temp;
             }
         }
 
-        void insert(int e, int p) {
-            Element* node = new Element(e,p);
+        void insert(T e, T p) {
+            Element<T>* node = new Element<T>(e,p);
             if(head == nullptr || head->priority < p) {
                 node -> next = head;
                 head = node;
             }else {
-                Element* current = head;
+                Element<T>* current = head;
                 while (current->next != nullptr && current->next->priority >= p) {
                     current = current->next;
                 }
@@ -46,42 +44,68 @@ class Queue_List {
             size++;
         }
 
-        int extract_max(){
-            int max = head -> value;
-            Element* temp = head -> next;
+        T extract_max(){
+            if (head == nullptr) {
+                cout << "kolejka jest pusta" << endl;
+                return -1;
+            }
+            T max = head -> value;
+            Element<T>* temp = head -> next;
             delete head;
             head = temp;
             size--;
             return max;
         }
 
-        int find_max() const{
-            int max = head -> value;
+        T find_max() const{
+            if (head == nullptr) {
+                cout << "kolejka jest pusta" << endl;
+                return;
+            }
+            T max = head -> value;
             return max;
         }
 
-        void modify_key(int e, int p) {
-            Element* temp = head;
-            Element* prev = nullptr;
+        void modify_key(T e, T p) {
+            Element<T>* temp = head;
+            Element<T>* prev = nullptr;
+            bool found = false;
             
             while(temp!=nullptr) {
                 if(temp->value == e){
+                    // usuniecie istniejacego elementu
                     if(prev==nullptr) {
                         head = temp->next;
                     }else{
                         prev -> next = temp-> next;
                     }
                     delete temp;
+                    found = true;
+                    size--; // zmniejszenie rozmiaru poniewaz fukcja insert go zwieksza
                     break;
                 }
                 prev = temp;
                 temp = temp->next;
             }
-            insert(e,p);
+            if(found) {
+                insert(e,p);
+            } else {
+                cout << "element o wartosci " << e << " nie zostal odnaleziony" << endl;
+            }
         }
 
-        int return_size() {
+        int return_size() const{
             return size;
+        }
+
+        void print() const{
+            Element<T>* current = head;
+            cout << "(value:priority): ";
+            while (current != nullptr) {
+                cout << current->value << ":" << current->priority << " ";
+                current = current->next;
+            }
+            cout << endl;
         }
     private:
         int size;
